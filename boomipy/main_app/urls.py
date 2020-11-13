68131ca84b50831b26b0057c9d9be844236e68f8
yiren <wang_yiren37@hotmail.com>
2020-11-13 16:56:42 -0500

    basic setup

--- boomipy/main_app/urls.py
+++ boomipy/main_app/urls.py
@@ -5,4 +5,13 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  # user log in
+  path('login/', views.login, name='login'),
+  #  landing page
+  path('landing/', views.landing, name='landing'),
+  # user playlists
+  path('myplaylist/', views.myplaylist, name='myplaylist'),
+  #  playlist details
+  path('details/', views.details, name='details'),
 ]
+
