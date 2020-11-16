76ac784928d0dac89d07d12b1ac9d4a6a2e5e678
yiren <wang_yiren37@hotmail.com>
2020-11-16 11:36:32 -0500

    manual merge

---
+++ boomipy/main_app/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+  path('', views.home, name='home'),
+  # user log in
+  path('login/', views.login, name='login'),
+  #  landing page
+  path('landing/', views.landing, name='landing'),
+  # user playlists
+  path('myplaylist/', views.myplaylist, name='myplaylist'),
+  #  playlist details
+  path('details/', views.details, name='details'),
+]
