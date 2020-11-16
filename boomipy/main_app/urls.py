c88cb79669e04a511f5c701b463cb557d1786de7
Prabhjeevan Kalra <prabhjeevan@MacBook-Pro.local>
2020-11-16 17:28:28 -0500

    login and signup style

--- boomipy/main_app/urls.py
+++ boomipy/main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.login, name='login'),
   # user log in
   path('accounts/', include('django.contrib.auth.urls')),
   path('accounts/signup', views.signup, name='signup'),
